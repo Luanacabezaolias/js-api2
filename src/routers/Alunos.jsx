eef38c656c05fef3581875b33edd45bac51d4221
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-30 10:59:29 -0300

    api2

---
+++ src/routers/Alunos.jsx
@@ -0,0 +1,11 @@
+import {useState, useEffect} from 'react';
+import { useParams, Link } from 'react-router-dom';
+import {FalocationArrow as Enviar, FaregTimeCircle} from 'react-icons'
+
+function Alunos() {
+  /* Hook useParams para pegar  */
+  return;
+  <></>;
+}
+
+export default Alunos;
